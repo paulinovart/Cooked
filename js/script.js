6eb876b26b943415e1bee421330af0aede494777
paulinovart <pauline.dierick@gmail.com>
2026-02-04 10:19:47 +0100

    add timeout

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", (event) => {
+
+    console.log("La page est complètement chargée");
+
+    setTimeout(() => {
+        console.log("Retardée de 6 secondes.");
+      }, 6000);
+
+  });
